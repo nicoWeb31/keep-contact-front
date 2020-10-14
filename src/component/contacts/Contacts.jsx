74ec7040f714096f66e5c39d3513b8cb484eb145
nicoWeb31 <nico.riot@free.fr>
2020-10-14 15:00:48 +0200

    csstransition et transition group

--- src/component/contacts/Contacts.jsx
+++ src/component/contacts/Contacts.jsx
@@ -1,24 +1,40 @@
-import React,{useContext} from 'react';
+import React, { useContext } from 'react';
 import ContactContext from '../../context/contact/ContactContext';
-import ContactItem from './ContactItem'
+import ContactItem from './ContactItem';
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 
 const Contacts = () => {
 
     const contactContext = useContext(ContactContext)
 
-    const {contacts,filtered} = contactContext;
+    const { contacts, filtered } = contactContext;
 
-    if(contacts.length === 0 ){
+    if (contacts.length === 0) {
         return <h4>Please add a contact</h4>
     }
 
     return (
         <React.Fragment>
+            <TransitionGroup>
 
-            {filtered !== null  ? filtered.map(contact=><ContactItem contact={contact} key={contact.id}/>) :
-            contacts.map(contact=><ContactItem contact={contact} key={contact.id}/>)
-            }
+                {filtered !== null ? filtered.map(contact =>
+                    <CSSTransition key={contact.id} timeout={500} classNames="item">
+                        <ContactItem contact={contact}  />
+                    </CSSTransition>
+                ) :
+
+
+                contacts.map(contact =>
+                    <CSSTransition key={contact.id} timeout={500} classNames="item">
+                        <ContactItem contact={contact}  />
+                    </CSSTransition>
+                )
+
+                }
+
+            </TransitionGroup>
+
 
         </React.Fragment>
     );
